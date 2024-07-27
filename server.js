e61570de74bd53f8aa3411ebdbde3227c83aba96
Tung Mai <tungm92@gmail.com>
2024-07-27 14:09:44 -0400

    fixed CSS issue

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ const passUsertoView = require('./middleware/pass-user-to-view');
 const authController = require('./controllers/auth.js');
 const eventsController = require('./controllers/events.js');
 
-const port = process.env.PORT ? process.env.PORT : '3000'
+const port = process.env.PORT ? process.env.PORT : '3000';
+const path = require('path');
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected on MongoDB ${mongoose.connection.name}.`);
 });
 
-// const _____ = require('./models/____.ejs)
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -35,7 +35,7 @@ app.use(
     }),
 );
 
-// css: app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(passUsertoView);
 
